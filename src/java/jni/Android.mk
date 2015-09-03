2febd338601a48188b077be0cbbb922022ed165c
Brian C Sparks <brian.c.sparks@hp.com>
2015-09-03 16:12:34 -0700

    Making JNI

--- src/java/jni/Android.mk
+++ src/java/jni/Android.mk
@@ -1,11 +1,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE 		:= libcurl
-LOCAL_SRC_FILES := libcurl.a
-
-include $(PREBUILT_STATIC_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE 		:= libcurl
+#LOCAL_SRC_FILES := libcurl.a
+#
+#include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
--- src/java/jni/mwp-jni.cpp
+++ src/java/jni/mwp-jni.cpp
@@ -1,21 +1,21 @@
 
 #include <jni.h>
 #include <android/log.h>
-#include "hp_mwp.h"
+//#include "hp_mwp.h"
 #include "mwp_types.hpp"
 #include "mwp_controller.hpp"
 #include <string>
 
 using std::string;
-using net_mobilewebprint::get_flag;
+//using net_mobilewebprint::get_flag;
 
-//using namespace net_mobilewebprint;
+////using namespace net_mobilewebprint;
 
-//jclass findClass(char const * name);
-//static JNIEnv *             g_env = NULL;
+////jclass findClass(char const * name);
+////static JNIEnv *             g_env = NULL;
 
-//static jstring to_jstring(JNIEnv *env, string const & str);
-//static jstring to_jstring(JNIEnv *env, char const * str);
+////static jstring to_jstring(JNIEnv *env, string const & str);
+////static jstring to_jstring(JNIEnv *env, char const * str);
 static string  to_string(JNIEnv * env, jstring jstr);
 
 extern "C" JNIEXPORT jstring JNICALL Java_net_mobilewebprint_Client_initJni(JNIEnv *env, jobject self)
@@ -25,14 +25,14 @@ extern "C" JNIEXPORT jstring JNICALL Java_net_mobilewebprint_Client_initJni(JNIE
 
 extern "C" JNIEXPORT void JNICALL Java_net_mobilewebprint_Client_logD(JNIEnv *env, jobject self, jstring tag, jstring msg)
 {
-//  if (get_flag("quiet")) { return; }
+////  if (get_flag("quiet")) { return; }
 
   __android_log_write(ANDROID_LOG_DEBUG, to_string(env, tag).c_str(), to_string(env, msg).c_str());
 }
 
 extern "C" JNIEXPORT void JNICALL Java_net_mobilewebprint_Client_logV(JNIEnv *env, jobject self, jstring tag, jstring msg)
 {
-//  if (!get_flag("verbose")) { return; }
+////  if (!get_flag("verbose")) { return; }
 
   __android_log_write(ANDROID_LOG_VERBOSE, to_string(env, tag).c_str(), to_string(env, msg).c_str());
 }
@@ -42,52 +42,52 @@ extern "C" JNIEXPORT void JNICALL Java_net_mobilewebprint_Client_logE(JNIEnv *en
   __android_log_write(ANDROID_LOG_ERROR, to_string(env, tag).c_str(), to_string(env, msg).c_str());
 }
 
-void net_mobilewebprint::log_d(char const * msg, log_param_t x)
-{
-  net_mobilewebprint::log_d(msg, "MobileWebPrint", x);
-}
-
-void net_mobilewebprint::log_d(char const * msg, char const * tag, log_param_t)
-{
-//  if (get_flag("quiet")) { return; }
-
-  __android_log_write(ANDROID_LOG_DEBUG, tag, msg);
-}
-
-void net_mobilewebprint::log_w(char const * msg, log_param_t)
-{
-//  if (get_flag("no_warn")) { return; }
-
-  __android_log_write(ANDROID_LOG_WARN, "MobileWebPrint", msg);
-}
-
-void net_mobilewebprint::log_v(char const * msg, log_param_t x)
-{
-  net_mobilewebprint::log_v(msg, "MobileWebPrint", x);
-}
-
-void net_mobilewebprint::log_v(char const * msg, char const * tag, log_param_t)
-{
-//  if (!get_flag("verbose")) { return; }
-
-  __android_log_write(ANDROID_LOG_VERBOSE, tag, msg);
-}
-
-void net_mobilewebprint::log_e(char const * msg, log_param_t)
-{
-  __android_log_write(ANDROID_LOG_ERROR, "MobileWebPrint", msg);
-}
-
-
-//jstring to_jstring(JNIEnv *env, string const & str)
+//void net_mobilewebprint::log_d(char const * msg, log_param_t x)
 //{
-//  return to_jstring(env, str.c_str());
+//  net_mobilewebprint::log_d(msg, "MobileWebPrint", x);
 //}
+
+//void net_mobilewebprint::log_d(char const * msg, char const * tag, log_param_t)
+//{
+//////  if (get_flag("quiet")) { return; }
 //
-//jstring to_jstring(JNIEnv *env, char const * str)
-//{
-//  return env->NewStringUTF(str);
+//  __android_log_write(ANDROID_LOG_DEBUG, tag, msg);
 //}
+
+//void net_mobilewebprint::log_w(char const * msg, log_param_t)
+//{
+//////  if (get_flag("no_warn")) { return; }
+//
+//  __android_log_write(ANDROID_LOG_WARN, "MobileWebPrint", msg);
+//}
+
+//void net_mobilewebprint::log_v(char const * msg, log_param_t x)
+//{
+//  net_mobilewebprint::log_v(msg, "MobileWebPrint", x);
+//}
+
+//void net_mobilewebprint::log_v(char const * msg, char const * tag, log_param_t)
+//{
+//////  if (!get_flag("verbose")) { return; }
+//
+//  __android_log_write(ANDROID_LOG_VERBOSE, tag, msg);
+//}
+
+//void net_mobilewebprint::log_e(char const * msg, log_param_t)
+//{
+//  __android_log_write(ANDROID_LOG_ERROR, "MobileWebPrint", msg);
+//}
+
+
+////jstring to_jstring(JNIEnv *env, string const & str)
+////{
+////  return to_jstring(env, str.c_str());
+////}
+////
+////jstring to_jstring(JNIEnv *env, char const * str)
+////{
+////  return env->NewStringUTF(str);
+////}
 
 string to_string(JNIEnv *env, jstring jstr) {
   string result;
@@ -99,53 +99,53 @@ string to_string(JNIEnv *env, jstring jstr) {
   return result;
 }
 
-//// From: http://stackoverflow.com/questions/13263340/findclass-from-any-thread-in-android-jni
+////// From: http://stackoverflow.com/questions/13263340/findclass-from-any-thread-in-android-jni
+//////
 ////
-//
-//JNIEnv * getEnv();
-//
-//JavaVM * g_jvm = NULL;
-//static jobject g_classLoader;
-//static jmethodID g_findClassMethod;
-//
-//extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM * pjvm, void * /*reserved*/)
-//{
-//  g_jvm = pjvm;
-//  JNIEnv * env = getEnv();
-//  jclass controllerClass = env->FindClass("net/printfromtheweb/mobile/Controller");
-//  jclass classClass = env->GetObjectClass(controllerClass);
-//
-//  jclass classLoaderClass = env->FindClass("java/lang/ClassLoader");
-//  jmethodID getClassLoaderMethod = env->GetMethodID(classClass, "getClassLoader", "()Ljava/lang/ClassLoader;");
-//
-//  g_classLoader = env->CallObjectMethod(controllerClass, getClassLoaderMethod);
-//  g_findClassMethod = env->GetMethodID(classLoaderClass, "findClass", "(Ljava/lang/String;)Ljava/lang/Class;");
-//
-//  return JNI_VERSION_1_6;
-//}
-//
-//jclass findClass(char const * name)
-//{
-//  jclass result = static_cast<jclass>(getEnv()->CallObjectMethod(g_classLoader, g_findClassMethod, getEnv()->NewStringUTF(name)));
-//
-//  if (getEnv()->ExceptionCheck()) {
-//    // TODO: tell the developer.
-//    getEnv()->ExceptionClear();
-//    return NULL;
-//  }
-//
-//  return result;
-//}
-//
-//JNIEnv * getEnv()
-//{
-//  JNIEnv * env = NULL;
-//  if (g_jvm->GetEnv((void**)&env, JNI_VERSION_1_6) >= 0) { return env; }
-//
-//  if (g_jvm->AttachCurrentThread(&env, NULL) >= 0)       { return env; }
-//
-//  return NULL;
-//}
+////JNIEnv * getEnv();
+////
+////JavaVM * g_jvm = NULL;
+////static jobject g_classLoader;
+////static jmethodID g_findClassMethod;
+////
+////extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM * pjvm, void * /*reserved*/)
+////{
+////  g_jvm = pjvm;
+////  JNIEnv * env = getEnv();
+////  jclass controllerClass = env->FindClass("net/printfromtheweb/mobile/Controller");
+////  jclass classClass = env->GetObjectClass(controllerClass);
+////
+////  jclass classLoaderClass = env->FindClass("java/lang/ClassLoader");
+////  jmethodID getClassLoaderMethod = env->GetMethodID(classClass, "getClassLoader", "()Ljava/lang/ClassLoader;");
+////
+////  g_classLoader = env->CallObjectMethod(controllerClass, getClassLoaderMethod);
+////  g_findClassMethod = env->GetMethodID(classLoaderClass, "findClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+////
+////  return JNI_VERSION_1_6;
+////}
+////
+////jclass findClass(char const * name)
+////{
+////  jclass result = static_cast<jclass>(getEnv()->CallObjectMethod(g_classLoader, g_findClassMethod, getEnv()->NewStringUTF(name)));
+////
+////  if (getEnv()->ExceptionCheck()) {
+////    // TODO: tell the developer.
+////    getEnv()->ExceptionClear();
+////    return NULL;
+////  }
+////
+////  return result;
+////}
+////
+////JNIEnv * getEnv()
+////{
+////  JNIEnv * env = NULL;
+////  if (g_jvm->GetEnv((void**)&env, JNI_VERSION_1_6) >= 0) { return env; }
+////
+////  if (g_jvm->AttachCurrentThread(&env, NULL) >= 0)       { return env; }
+////
+////  return NULL;
+////}
 
 
 
