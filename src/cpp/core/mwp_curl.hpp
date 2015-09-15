569a1245d5daf7787d2e9716ea8891b4646f8d8b
Brian C Sparks <brian.c.sparks@hp.com>
2015-09-15 11:46:58 -0700

    Adding cURL to 1.1b

--- src/cpp/core/mwp_curl.hpp
+++ src/cpp/core/mwp_curl.hpp
@@ -46,6 +46,23 @@ namespace net_mobilewebprint {
     virtual mq_result        on_select_loop_idle(select_loop_idle_extra_t const &  extra);
   };
 
+  struct curl_mini_connection_t : public mq_handler_t
+  {
+    curl_mini_connection_t(curl_mini_t &);
+
+    // ----- Hooking into the select loop -----
+    virtual mq_result                 initialize();
+
+    virtual mq_result       on_select_loop_start(select_loop_start_extra_t const & extra);
+
+    virtual mq_result              on_pre_select(pre_select_extra_t & extra);
+    virtual mq_result                  on_select(select_extra_t & extra);
+    virtual mq_result                 on_message(string const & name, buffer_view_t const & payload, message_extra_t & extra);
+
+    virtual mq_result         on_select_loop_end(select_loop_end_extra_t const &   extra);
+    virtual mq_result        on_select_loop_idle(select_loop_idle_extra_t const &  extra);
+  };
+
 #if HAVE_CURL
   struct curl_t : public curl_base_t
   {
